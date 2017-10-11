998f33df80ce0681d64304c3a8ff42a52632a2a0
Nils <nilsl@student.ethz.ch>
2017-10-11 12:01:39 +0200

    Change grabbing functionality to use Xlib so my mouse pointer isn't eaten. Time for a passive-aggressive comment about xcb.

--- airhorn.c
+++ airhorn.c
@@ -3,35 +3,70 @@
 void setup(xcb_connection_t *connection) {
 	xcb_generic_error_t *err;
 	xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
-	xcb_grab_pointer_cookie_t grab_cookie = xcb_grab_pointer(connection, True, screen->root, XCB_EVENT_MASK_BUTTON_PRESS | XCB_EVENT_MASK_BUTTON_RELEASE, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_TIME_CURRENT_TIME);
-	xcb_grab_pointer_reply_t *reply = xcb_grab_pointer_reply(connection, grab_cookie, &err);
-	if (reply->status != XCB_GRAB_STATUS_SUCCESS) {
-		perror("could not grab pointer");
-		exit(1);
-	}
+	//xcb_void_cookie_t grab_cookie = xcb_grab_button(connection, True, screen->root, XCB_NONE, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC, XCB_NONE, XCB_NONE, XCB_BUTTON_INDEX_1, XCB_MOD_MASK_ANY);
+	//xcb_generic_error_t *error = xcb_request_check(connection, grab_cookie);
+	//if (error != NULL) {
+	//	xcb_disconnect(connection);
+	//	perror("could not subscribe to events on a window, bailing out");
+	//	exit(1);
+	//}  
+	//status = XGrabPointer(dpy, root, False,
+	//		ButtonPressMask|ButtonReleaseMask, GrabModeSync,
+	//		GrabModeAsync, root, cursor, CurrentTime);
+	//if (status != GrabSuccess) {
+	//	fprintf(stderr, "%s: Can't grab the mouse.\n", ProgramName);
+	//	exit(1);
+	//}
+
+	/* Let the user select a window... */
+
+	//free(error);
         xcb_flush(connection);
 }
 
 int main(int argc, char *argv[]) {
+	XEvent event;
 	xcb_generic_event_t *e;
 	Display *dpy = XOpenDisplay(NULL);
 	xcb_connection_t *connection = XGetXCBConnection(dpy);
-	setup(connection);
-	while ((e = xcb_wait_for_event(connection))) {
-		switch(e->response_type & ~0x80) {
-			case XCB_BUTTON_PRESS:
-				printf("It is work.\n");
+	//setup(connection);
+	xcb_screen_t *screen = xcb_setup_roots_iterator(xcb_get_setup(connection)).data;
+	u_int32_t status = XGrabPointer(dpy, screen->root, False,
+			ButtonPressMask|ButtonReleaseMask, GrabModeSync,
+			GrabModeAsync, screen->root, 0, CurrentTime);
+	if (status != GrabSuccess) {
+		perror("can't grab.\n");
+		exit(1);
+	}
+
+	XAllowEvents(dpy, SyncPointer, CurrentTime);
+	do {
+		/* allow one more event */
+		XAllowEvents(dpy, SyncPointer, CurrentTime);
+		XWindowEvent(dpy, screen->root, ButtonPressMask|ButtonReleaseMask, &event);
+		switch (event.type) {
+			case ButtonPress:
+				printf("Hey.\n");
 				break;
-			case XCB_KEY_PRESS:
-				printf("go key\n");
-				break;
-			case XCB_MOTION_NOTIFY:
-				printf("pointer motion\n");
-				break;
-			default:
+			case ButtonRelease:
 				break;
 		}
-		free(e);
-	}
-	xcb_ungrab_pointer(connection, XCB_TIME_CURRENT_TIME);
+	} while(1);
+	//while ((e = xcb_wait_for_event(connection))) {
+	//	switch(e->response_type & ~0x80) {
+	//		case XCB_BUTTON_PRESS:
+	//			printf("It is work.\n");
+	//			break;
+	//		case XCB_KEY_PRESS:
+	//			printf("go key\n");
+	//			break;
+	//		case XCB_MOTION_NOTIFY:
+	//			printf("pointer motion\n");
+	//			break;
+	//		default:
+	//			break;
+	//	}
+	//	free(e);
+	//}
+	//xcb_ungrab_pointer(connection, XCB_TIME_CURRENT_TIME);
 }
